88194d20946012e3358b99f976f18c0b563bfa17
Salem Nabeel Salem <isalemnabeelsalem@gmail.com>
2026-03-27 18:30:32 +0400

    Refactor Vite configuration to use import.meta.env for base path.

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-let process = process.env.VITE_BASE_PATH = undefined;
-
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
 
-  base: process.env.VITE_BASE_PATH || "/budget-buddy-application-frontend",
+  base: import.meta.env.VITE_BASE_PATH || "/budget-buddy-application-frontend",
 })
