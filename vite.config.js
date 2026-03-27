ea70f1b39470d54b3f33d505d09126d1f6a6be30
Salem Nabeel Salem <isalemnabeelsalem@gmail.com>
2026-03-27 18:26:49 +0400

    Set base path for Vite configuration to support environment variable.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+let process = process.env.VITE_BASE_PATH = undefined;
+
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
+
+  base: process.env.VITE_BASE_PATH || "budget-buddy-application-frontend",
 })
